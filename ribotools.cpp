a93329fe3b9d5455d511972657edecc55f37054b
Georgi Tushev <georgi.tushev@brain.mpg.de>
2018-12-04 23:28:07 +0100

    add tabix support

--- ribotools.cpp
+++ ribotools.cpp
@@ -5,20 +5,58 @@
 #include "version.hpp"
 #include "metagene.hpp"
 
+#include <htslib/tbx.h>
+
 int help(void);
 
 int main(int argc, char const *argv[])
 {
     std::string fileName = "/Users/tushevg/Desktop/RiboData/gbed/MonoVsPoly/Total_01.gbed.gz";
+    std::string query = "chr18:56193977-56295869";
+
+
 
     auto tic = std::chrono::high_resolution_clock::now();
 
+    htsFile *fp = hts_open(fileName.c_str(), "r");
+    if (!fp)
+    {
+        std::cerr << "ERROR: could not read hts file." << std::endl;
+        return 1;
+    }
 
+    tbx_t* tbx = tbx_index_load(fileName.c_str());
+    if (!tbx)
+    {
+        std::cerr << "ERROR: could not read index." << std::endl;
+        return 1;
+    }
+
+    // create iterator
+    hts_itr_t *iter = tbx_itr_querys(tbx, query.c_str());
+    if (!iter)
+    {
+        std::cerr << "ERROR: could not query the region." << std::endl;
+        return 1;
+    }
+
+    kstring_t str;
+    while (tbx_itr_next(fp, tbx, iter, &str) >= 0)
+    {
+        std::string line(str.s);
+        //std::cout << "Line: " << line << std::endl;
+        iter->i++;
+    }
+    
+
+    free(ks_release(&str));
+    hts_close(fp);
+    tbx_itr_destroy(iter);
+    tbx_destroy(tbx);
     //BGZF *fh = bgzf_open(fileName.c_str(), "r");
     //bgzf_close(fh); 
 
-    regidx_t *fh = regidx_init(fileName.c_str(), MetaGene::tabixParse, MetaGene::tabixFree, sizeof(char *), NULL);
-    regidx_destroy(fh);
+    
 
     auto toc = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = toc - tic;
