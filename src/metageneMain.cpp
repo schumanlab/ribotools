21a60ba873859b8d0434460d509a04e0d184e91a
Georgi Tushev <georgi.tushev@brain.mpg.de>
2018-12-05 15:46:21 +0100

    add metagene main

---
+++ src/metageneMain.cpp
@@ -0,0 +1,170 @@
+#include "metagene.hpp"
+
+class MetaGeneConfig
+{
+public:
+    MetaGeneConfig(int argc = 0, char const *argv[] = nullptr);
+
+    bool errorFlag;
+    int nbins;
+    float boundLower;
+    float boundUpper;
+    std::string fileBed;
+    std::vector<std::string> filesGbed;
+
+    void help(void);
+    void error(const std::string &message);
+};
+
+
+int metageneMain(int argc, char const *argv[])
+{
+    auto config = MetaGeneConfig(argc, argv);
+
+    std::cout << config.filesGbed.size() << std::endl;
+
+    return 0;
+}
+
+
+MetaGeneConfig::MetaGeneConfig(int argc, char const *argv[]) :
+        errorFlag(false),
+        nbins(100),
+        boundLower(-0.3),
+        boundUpper(1.3),
+        fileBed("")
+{
+    // parse parameters
+    bool requiredBed = false;
+    bool requiredGbed = false;
+    int p = 1;
+    while (p < argc)
+    {
+        // next key
+        std::string argumentKey(argv[p]);
+        if (argumentKey.at(0) != '-')
+        {
+            error("provided argument '" + argumentKey + "' is not a valid key."); 
+            return;   
+        }
+
+        // check for keys without a value
+        if ((argumentKey == "-h") || (argumentKey == "--help"))
+        {
+            help();
+            return;
+        }
+        else if ((argumentKey == "-v") || (argumentKey == "--version"))
+        {
+            version("ribotools metagene");
+            return;
+        }
+        else if ((argumentKey == "-c") || (argumentKey == "--contact"))
+        {
+            contact();
+            return;
+        }
+
+        // increment parameter index
+        p++;
+        if (p >= argc)
+        {
+            error("provided key '" + argumentKey + "' requires a value.");
+            return;
+        }
+
+        // check for keys with value
+        std::string argumentValue(argv[p]);
+        if ((argumentKey == "-a") || (argumentKey == "--bed"))
+        {
+            fileBed = argumentValue;
+            requiredBed = true;
+        }
+        else if ((argumentKey == "-g") || (argumentKey == "--gbed"))
+        {
+            filesGbed.push_back(argumentValue);
+            int n = p + 1;
+            while( n < argc)
+            {
+                std::string argumentValueNext(argv[n]);
+                if (argumentValueNext.at(0) == '-')
+                    break;
+                filesGbed.push_back(argumentValueNext);
+                n++;
+            }
+            p += filesGbed.size() - 1;
+            requiredGbed = true;
+        }
+        else if ((argumentKey == "-n") || (argumentKey == "--nbins"))
+        {
+            nbins = std::stoi(argumentValue);
+        }
+        else if ((argumentKey == "-l") || (argumentKey == "--lower"))
+        {
+            boundLower = std::stof(argumentValue);
+        }
+        else if ((argumentKey == "-u") || (argumentKey == "--upper"))
+        {
+            boundUpper = std::stof(argumentValue);
+        }
+        else
+        {
+            error("unknown key '" + argumentKey + "'.");
+            return;
+        }
+        p++;
+    }
+
+    // check required parameters
+    if (requiredBed == false)
+    {
+        error("required annotation file is missing.");
+        return;
+    }
+
+    if (requiredGbed == false)
+    {
+        error("required coverage file is missing.");
+        return;
+    }
+
+};
+
+
+void MetaGeneConfig::help()
+{
+    std::cerr << std::endl;
+    std::cerr << "Tool: ribotools metagene" << std::endl;
+    version("Version: ");
+    std::cerr << std::endl;
+    std::cerr << "Summary: creates coverage histogram by binning transcripts length." << std::endl;
+    std::cerr << std::endl;
+    std::cerr << "Usage: ribotools metagene [OPTIONS] -a <bed> -g <gbeds>" << std::endl;
+    std::cerr << std::endl;
+    std::cerr << "Required:" << std::endl;
+    std::cerr << '\t' << "-a | --bed    annotation file in BED12 format." << std::endl;
+    std::cerr << '\t' << "-g | --gbed   coverage files in GraphBed format." << std::endl;
+    std::cerr << std::endl;
+    std::cerr << "Options:" << std::endl;
+    std::cerr << '\t' << "-n | --nbins  number of histogram bins [100]." << std::endl;
+    std::cerr << '\t' << "-l | --lower  lower histogram bound [-0.3]." << std::endl;
+    std::cerr << '\t' << "-u | --upper  upper histogram bound [1.3]." << std::endl;
+    std::cerr << '\t' << "-d | --depth  minimum depth [20]." << std::endl;
+    std::cerr << std::endl;
+    std::cerr << "General help:" << std::endl;
+    std::cerr << '\t' << "-h | --help" << '\t' << "Print this help menu." << std::endl;
+    std::cerr << '\t' << "-v | --version" << '\t' << "What version of ribotools are you using?" << std::endl;
+    std::cerr << '\t' << "-c | --contact" << '\t' << "Feature requests, bugs, mailing lists, etc." << std::endl;
+    std::cerr << std::endl;
+}
+
+
+void MetaGeneConfig::error(const std::string &message)
+{
+    std::cerr << std::endl;
+    std::cerr << "ribotools metagene" << std::endl;
+    std::cerr << '\t' << "Error: " << message << std::endl;
+    errorFlag = true;
+    help();
+}
+
--- src/ribotools.cpp
+++ src/ribotools.cpp
@@ -1,57 +1,52 @@
 #include <iostream>
 
 #include "version.hpp"
-#include "metagene.hpp"
 
-
-
-int help(void);
+int help();
+int metageneMain(int argc, char const *argv[]);
 
 int main(int argc, char const *argv[])
 {
 
-    /*
-    // check for sub-command
-    if (argc < 2) 
+    // no sub-commands
+    if (argc == 1)
         return help();
-    
-    // parse sub-command
+
+    // check first argument for subcommand
     std::string subCommand(argv[1]);
 
     if ((subCommand == "-h") || (subCommand == "--help"))
+    {
         return help();
-
+    }
     else if ((subCommand == "-v") || (subCommand == "--version"))
+    {
         return version("ribotools");
-        
+    }
     else if ((subCommand == "-c") || (subCommand == "--contact"))
+    {
         return contact();
-
+    }
     else if (subCommand == "metagene")
     {
-        auto obj = MetaGene();
-        if(!obj.parse(argc - 1, argv + 1))
-            return 0;
-        obj.pileup();
-
-        
+        return metageneMain(argc - 1, argv + 1);
     }
     else
     {
         std::cerr << "ribotools" << std::endl;
         std::cerr << '\t' << "Error:: unknown subcommand " << subCommand << std::endl;
-        std::cerr << std::endl;
         return help();
     }
-    */
 
     return 0;
 }
 
 int help()
 {
+    std::cerr << std::endl;
     std::cerr << "ribotools is a toolset for ribosome footprint analysis." << std::endl;
     version("Version: ");
+    std::cerr << std::endl;
     std::cerr << "About: developed in the Scientific Computing Facility at Max-Planck Instittue For Brain Research." << std::endl;
     std::cerr << "Docs: https://ribotools.github.molgen.mpg.de" << std::endl;
     std::cerr << "Code: https://github.molgen.mpg.de/MPIBR-Bioinformatics/ribotools" << std::endl;
