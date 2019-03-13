8642f6e630f23b1c48aa096b6c7850b6afdbbf22
Georgi Tushev <georgi.tushev@brain.mpg.de>
2019-03-13 14:30:11 +0100

    adapt PlotPSites to new table

--- plotting/PlotPSites.m
+++ plotting/PlotPSites.m
@@ -1,124 +1,126 @@
-%% plotPSites
+%% PlotPSites
 clc
 clear variables
 close all
 
-fh = fopen('/Users/tushevg/Desktop/testTrackCrct.txt','r');
-txt = textscan(fh, '%s %s %s','delimiter','\t');
-fclose(fh);
-fname = txt{1};
-val = txt{2};
-cnt = txt{3};
 
-Y = zeros(length(val), 300);
-Yc = zeros(length(cnt), 300);
-idxStart = (1:100);
-idxCenter = (101:200);
-idxEnd = (201:300);
-for k = 1 : length(val)
-    
-    v = sscanf(val{k}, '%f,');
-    c = sscanf(cnt{k}, '%f,');
-    Y(k,idxStart) = v(idxStart);%./mean(v(idxStart));
-    Y(k,idxCenter) = v(idxCenter);%./mean(v(idxCenter));
-    Y(k,idxEnd) = v(idxEnd);%./mean(v(idxEnd));
-    
-    Yc(k,idxStart) = c(idxStart);%./mean(v(idxStart));
-    Yc(k,idxCenter) = c(idxCenter);%./mean(v(idxCenter));
-    Yc(k,idxEnd) = c(idxEnd);%./mean(v(idxEnd));
-    
-end
-
-%Y = bsxfun(@rdivide, Y, mean(Yc,2));
+%% read data
+data = readPSiteTable('/Users/tushevg/Desktop/Periodicity/resultPeriodicity_12Mar2019.txt');
+idx = false(length(data.file), 1);
+idx(4:6) = true;
 
 
+%% Plot Psites
+Y = data.position(idx,:);
+Y = Y ./ mean(Y(:));
+Yavg = mean(Y, 1);
+Yste = std(Y, [], 1) ./ sqrt(sum(idx));
+X = (-25:75);
 
-idx = 1:3;
-titleSample = 'Neuropil-Mono';
+xTickLabel = arrayfun(@(x) {sprintf('%d',x)},[(-25:25:75),(-50:25:50),(-75:25:25)]);
+xTickLabel(2) = {'start'};
+xTickLabel(8) = {'center'};
+xTickLabel(14) = {'end'};
 
-idxXTemplate = (0:99);
-idxX = [idxXTemplate - 25;...
-        idxXTemplate + 91;...
-        idxXTemplate + 207];
-idxY = [1:100;...
-        101:200;...
-        201:300];
-  
-clr_mtx = [30,144,255;...
-           148,0,211;...
-           144,238,144]./255;
-
-figure('color','w');
+figure('Color','w');
 hold on;
-for k = 1 : 3
+xTickStart = plotTrack(X, Yavg(1:101), Yste(1:101));
+xTickCenter = plotTrack(X + 122, Yavg(102:202), Yste(102:202));
+xTickEnd = plotTrack(X + 244, Yavg(203:303), Yste(203:303));
+hold off;
+set(gca, 'box','off',...
+        'xtick',[xTickStart, xTickCenter, xTickEnd],...
+        'xticklabel',xTickLabel,...
+        'ylim',[0,9],...
+        'ytick',(0:8));
+xlabel('relative position');
+ylabel('normalized p-site coverage');
+
+
+%% Plot Frame
+pos = get(gca,'Position');
+pos(1) = pos(1) + pos(3)/1.5;
+pos(2) = pos(2) + pos(4)/1.6;
+pos(3) = pos(3)/4;
+pos(4) = pos(4)/3;
+
+
+result = (data.frame(idx,:)-(1/3))./(1/3);
+resultAvg = mean(result, 1);
+resultSte = std(result,[],1)./sqrt(sum(idx));
+p = anova1(result,[],'off');
+
+ax = axes('Position',pos);
+hold(ax,'on');
+bar(ax, 0, resultAvg(1), 0.4,'facecolor',[.1,.1,.1]);
+bar(ax, [1,2], resultAvg(2:3),0.4,'facecolor',[.45,.45,.45]);
+plot([0,0],[resultAvg(1),resultAvg(1)+resultSte(1)],'k','linewidth',1.2);
+plot([1,1],[resultAvg(2),resultAvg(2)-resultSte(2)],'k','linewidth',1.2);
+plot([2,2],[resultAvg(3),resultAvg(3)-resultSte(3)],'k','linewidth',1.2);
+
+hold(ax,'off');
+set(ax,'xtick',(0:2),'xlim',[-0.5,2.5],'ylim',[-0.6,0.6],'ytick',(-0.5:0.5:0.5));
+text(ax,1,0.6,sprintf('anova p = %.4e',p),'verticalalignment','bottom','horizontalalignment','center');
+xlabel(ax,'frame');
+ylabel(ax,'(observed - expected) / expected');
+
+
+print(gcf,'-dsvg','-r300','figurePeriodicity_NplPoly_13Mar2019.svg');
+
+
+
+
+%% FUNCTIONS
+function xTick = plotTrack(X, Yavg, Yste)
     
-    Ym = mean(Y(idx,idxY(k,:)), 1);
-    Yse = std(Y(idx,idxY(k,:)), [], 1);
-    Xbin = idxX(k,:);
-    xpatch = [Xbin, fliplr(Xbin)];
-    ypatch = [(Ym-Yse), fliplr((Ym+Yse))];
+    Ctrack = [.5, .5, .5];
+    Cframe0 = [.1, .1, .1];
+    Cframe1 = [.45, .45, .45];
+    Cframe2 = [.45, .45, .45];
     
-    hp = fill(xpatch, ypatch, clr_mtx(k,:));
-    set(hp,'edgecolor','none','facealpha',0.5);
-    plot(Xbin, Ym, 'color',clr_mtx(k,:));
-    plot(Xbin, Ym, '.', 'color', clr_mtx(k,:), 'markersize', 8);
+    Xpatch = [X, fliplr(X)];
+    Ypatch = [(Yavg - Yste), fliplr(Yavg + Yste)];
+    hp = fill(Xpatch, Ypatch, Ctrack);
+    set(hp, 'edgecolor', 'none', 'facealpha', .2);
+    plot(X, Yavg, 'color', Ctrack);
+    idxFrame0 = mod(X,3) == 0;
+    idxFrame1 = mod(X,3) == 1;
+    idxFrame2 = mod(X,3) == 2;
+    
+    plot(X(idxFrame0), Yavg(idxFrame0), '.', 'markersize', 5, 'color', Cframe0);
+    plot(X(idxFrame1), Yavg(idxFrame1), '.', 'markersize', 3, 'color', Cframe1);
+    plot(X(idxFrame2), Yavg(idxFrame2), '.', 'markersize', 3, 'color', Cframe2);
+    xTick = (X(1):25:X(end));
+
+end
+
+
+
+function data = readPSiteTable(fileName)
+
+    fh = fopen(fileName, 'r');
+    txt = textscan(fh, '%s %s %s %s %s', 'delimiter', '\t');
+    fclose(fh);
+    data.file = txt{1};
+    frame = txt{2};
+    positionStart = txt{3};
+    positionCenter = txt{4};
+    positionEnd = txt{5};
+    
+    n = length(data.file);
+    data.frame = zeros(n, 3);
+    data.position = zeros(n, 303);
+    
+    
+    for k = 1 : n
+        data.frame(k,:) = sscanf(frame{k}, '%f,');
+        data.position(k,1:101) = sscanf(positionStart{k}, '%f,');
+        data.position(k,102:202) = sscanf(positionCenter{k}, '%f,');
+        data.position(k,203:303) = sscanf(positionEnd{k}, '%f,');
+    end
+    
+    %% normalize
+    data.frame = bsxfun(@rdivide, data.frame, sum(data.frame, 2));
+    data.position = bsxfun(@rdivide, data.position, mean(data.position, 2));
     
 end
-hold off;
-
-xtickTemplate = (0:25:100);
-
-
-xtick = [xtickTemplate - 25,...
-         xtickTemplate + 91,...
-         xtickTemplate + 207];
-xticklabel = num2cell([(-25:25:75),...
-              (-50:25:50),...
-              (-75:25:26)]);
-xticklabel(2) = {'start'};
-xticklabel(8) = {'center'};
-xticklabel(14) = {'stop'};
-          
-set(gca,'xtick',xtick,...
-        'xticklabel',xticklabel,...
-        'ylim',[0,max(Y(:))],'box','on');
-xlabel('relative offset [nts]','fontsize',12);
-ylabel('p-site depth','fontsize',12);
-title(titleSample,'fontsize',12,'fontweight','normal');
-print(gcf, '-dsvg','-r300',sprintf('figurePSiteProjection_%s.svg',titleSample));
-
-nfft = 256; % next larger power of 2
-Fs = 1;
-
-Xff = (0:nfft/2)* Fs/nfft;
-Yff = zeros(length(idx),ceil(nfft/2+1));
-for k = 1 : length(idx)
-    Yn = mean(Y(idx(k),:), 1);
-    Yn = [Yn(1:100),Yn(103:200),Yn(201:300)];
-    Yz = Yn - mean(Yn);
-
-    Yf = fft(Yz,nfft); % Fast Fourier Transform
-    Yp = abs(Yf.^2); % raw power spectrum density
-    
-    
-    Yff(k,:) = Yp(1:1+nfft/2); % half-spectrum
-    
-end
-
-Ym = mean(Yff,1);
-Yse = std(Yff,[],1)./sqrt(size(Yff,1));
-xpatch = [Xff, fliplr(Xff)];
-ypatch = [(Ym-Yse), fliplr((Ym+Yse))];
-Xexp = 1/3;
-figure('color','w');
-hold on;
-hp = fill(xpatch, ypatch, [0.5,0.5,0.5]);
-set(hp,'edgecolor','none','facealpha',0.5);
-plot(Xff, Ym, 'color','k');
-ylim = get(gca,'ylim');
-plot([Xexp,Xexp],[0,0.025*max(ylim)],'r','linewidth',1.2);
-hold off;
-ylabel('power spectrum desnity','fontsize',12);
-xlabel('frequency [Hz]');
-title(titleSample,'fontsize',12,'fontweight','normal');
-print(gcf, '-dsvg','-r300',sprintf('figurePSiteFTransform_%s.svg',titleSample));
