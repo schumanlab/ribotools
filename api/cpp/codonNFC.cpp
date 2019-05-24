541779c6fd180b0ee1588a4d12acca21ae6daf39
Georgi Tushev <georgi.tushev@brain.mpg.de>
2019-05-24 13:28:50 +0200

    add codon table

--- api/cpp/codonNFC.cpp
+++ api/cpp/codonNFC.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <unordered_map>
 
 #include <htslib/sam.h>
 #include <htslib/hts.h>
@@ -10,12 +11,23 @@
 #include "bedrecord.hpp"
 
 
+struct AAIndex {
+    int index;
+    char code;
+};
+
+void loadCodonMap(std::unordered_map<std::string, AAIndex> &codonMap, const std::string &fileCodonTable);
 
 
 int main(int argc, char *argv[])
 {
-    auto fileBed = std::string(argv[1]);
-    auto fileBam = std::string(argv[2]);
+    auto fileCodonMap = std::string(argv[1]);
+    auto fileBed = std::string(argv[2]);
+    auto fileBam = std::string(argv[3]);
+
+    // Load Codon Map
+    std::unordered_map<std::string, AAIndex> codonMap;
+    loadCodonMap(codonMap, fileCodonMap);
 
     std::ifstream fh;
     std::string line;
@@ -40,14 +52,14 @@ int main(int argc, char *argv[])
         std::cout << bed.transcript() << std::endl;
         int queryStart = static_cast<int>(bed.cdsStart);// + 60;
         int queryEnd = static_cast<int>(bed.cdsEnd);// - 60;
-
+        
 
         int tid = bam_name2id(hdrBam, bed.transcript().c_str());
         iter = sam_itr_queryi(idxBam, tid, queryStart, queryEnd);
         int ret = 0;
         while (sam_itr_next(fhBam, iter, aln) >= 0) {
             ret++;
-            int readStart = aln->core.pos;
+            int readStart = aln->core.pos + 15;
             int readOffset = readStart - bed.cdsStart;
             int readLength = bam_cigar2qlen(aln->core.n_cigar, bam_get_cigar(aln));
             
@@ -55,6 +67,8 @@ int main(int argc, char *argv[])
                 readSeq[b] = seq_nt16_str[bam_seqi(bam_get_seq(aln), b)];
             }
             readSeq[aln->core.l_qseq] = '\0';
+
+            if (0 <= readStart) && (readStart <= )
             std::cout << readStart << "\t" << readOffset << "\t" << readLength << "\t" << readSeq << std::endl;
             
         }
@@ -73,4 +87,24 @@ int main(int argc, char *argv[])
     sam_close(fhBam);
 
     return 0;
+}
+
+
+void loadCodonMap(std::unordered_map<std::string, AAIndex> &codonMap, const std::string &fileCodonTable)
+{
+    std::ifstream fh;
+    int index = 0;
+    std::string line;
+
+    fh.open(fileCodonTable);
+    while (std::getline(fh, line)) {
+        std::istringstream isline(line);
+        std::string aaKey;
+        char aaValue;
+        isline >> aaKey;
+        isline >> aaValue;
+        codonMap[aaKey] = {index, aaValue};
+        index++;
+    }
+    fh.close();
 }
