cd4125c1b532e6cd14adfa9fc9c042c86dff11a6
Georgi Tushev <georgi.tushev@brain.mpg.de>
2019-11-26 15:02:43 +0100

    add filter based on orf length

--- main_depth.cpp
+++ main_depth.cpp
@@ -6,6 +6,22 @@
 #include "bamio.h"
 #include "version.h"
 
+std::vector<int> reduceToCodons(std::vector<int> &depth, std::size_t size) {
+    std::vector<int> codons(size, 0);
+    std::vector<int>::iterator ov = codons.begin();
+    for (std::vector<int>::const_iterator it = depth.begin();
+         it != depth.end(); it += 3) {
+
+        if (ov != codons.end()) {
+            *ov = *it + *(it + 1) + *(it + 2);
+            ++ov;
+        }
+    }
+    return codons;
+}
+
+
+
 int main_depth(const int argc, const char *argv[])
 {
     std::string fileBed;
@@ -42,17 +58,53 @@ int main_depth(const int argc, const char *argv[])
         return EXIT_FAILURE;
     }
 
-    std::cout << hBam.aux.size() << std::endl;
-    for (auto handle : hBam.aux) {
-        std::cout << handle->name() << "\t" << handle->count() << std::endl;
+    std::size_t sizeColumns = hBam.aux.size();
+
+    // loop over BED records
+    while (hBed.next()) {
+
+        // size in codons
+        std::size_t sizeRows = static_cast<std::size_t>(hBed.bed().orfSpan() / 3);
+        if (sizeRows < 50) continue;
+
+
+        // calculate counts
+        std::size_t useFlag = 0;
+        for (auto handle : hBam.aux) {
+                handle->query(hBed.bed().name(1), hBed.bed().orfStart(), hBed.bed().orfEnd());
+                int count = handle->count();
+                float avgDepth = static_cast<float>(count) / hBed.bed().orfSpan();
+                if (avgDepth >= 0.1f) useFlag++;
+        }
+
+        // filter based on minimum ratio in all replica
+        if (useFlag < 4) continue;
+
+        // depth per replica
+        std::vector<int> vDepth(sizeRows, 0);
+        std::vector<std::vector<int>> vData(sizeColumns, vDepth);
+        std::vector<std::vector<int>>::iterator iv = vData.begin();
+        for (auto handle : hBam.aux) {
+            std::vector<int> depthNucleotides = handle->depth(hBed.bed().name(1), hBed.bed().orfStart(), hBed.bed().orfEnd());
+            std::vector<int> depthCodons = reduceToCodons(depthNucleotides, sizeRows);
+            if (iv != vData.end()) {
+                *iv = depthCodons;
+                ++iv;
+            }
+        }
+
+        // print loop
+        for (std::size_t r = 0; r < sizeRows; ++r) {
+
+            std::cout << hBed.bed().name(2) << "\t" << r;
+            for (std::size_t c = 0; c < sizeColumns; ++c) {
+                std::cout << "\t" << vData[c][r];
+            }
+            std::cout << std::endl;
+
+        }
+
     }
-
-
-
-
-
-
-
 
 
     return 0;
